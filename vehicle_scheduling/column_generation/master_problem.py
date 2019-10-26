2a9998d6dfb349bef57562e357a51aff05f22ccc
Eder Vasco Pinheiro <eder.pinheiro@wplex.com.br>
2019-10-26 17:33:14 -0300

    master problem

--- vehicle_scheduling/column_generation/master_problem.py
+++ vehicle_scheduling/column_generation/master_problem.py
@@ -5,41 +5,61 @@ import logging
 
 class Variable:
 
-    def __init__(self, cost: int, commodity):
+    def __init__(self, cost: int, commodity: str, scheduled_trips):
+        # self.index = None
         self.cost = cost
         self.commodity = commodity
+        self.scheduled_trips = scheduled_trips
 
 
-class SetParticioning:
+class SetPartitioning:
 
-    def __init__(self, initial_variables: map[str], trips):
+    def __init__(self, initial_variables_by_commodity, scheduled_trips):
         logging.getLogger('pyomo.core').setLevel(logging.ERROR)
         self.solver = pyo.opt.SolverFactory('cbc')
         self.results = "NotExecuted"
-        self.model = pe.ConcreteModel()
-
-        self.model.J = range(initial_variables)
-        self.model.x = pe.Var(self.model.J, domain=pe.NonNegativeReals)
-        self.cost_vector = [v.cost for v in initial_variables]
-        self.model.obj = pe.Objective(expr=sum(self.cost_vector[i] * self.model.x[i]
-                                               for i in self.model.J), sense=pe.maximize)
+        self.model = pe.ConcreteModel('master_problem')
+        indexes = []
+        self.cost_vector = {}
+        for commodity in initial_variables_by_commodity.keys():
+            for index, var in enumerate(initial_variables_by_commodity[commodity]):
+                var.index = (commodity, str(index))
+                indexes.append(var.index)
+                self.cost_vector[var.index] = var.cost
+        self.model.DK = indexes
+        self.model.var_lambda = pe.Var(self.model.DK, domain=pe.NonNegativeReals)
+        self.model.obj = pe.Objective(expr=sum(self.cost_vector[dk] * self.model.var_lambda[dk]
+                                               for dk in self.model.DK), sense=pe.minimize)
         self.model.dual = pe.Suffix(direction=pe.Suffix.IMPORT)
-        self.model.rc = pe.Suffix(direction=pe.Suffix.IMPORT_EXPORT)
-        self.model.constraints = pe.ConstraintList()
-        # self.model.constraints.add(expr=6.7*self.model.x[0] + 2 * self.model.x[1] <= 17)
-        # self.model.constraints.add(expr=-2.5*self.model.x[0] + self.model.x[1] <= 2)
-        self.model.constraint_row1 = pe.Constraint(expr=8 * self.model.x[0] + 2 * self.model.x[1] <= 17)
-        self.model.constraint_row2 = pe.Constraint(expr=-self.model.x[0] + self.model.x[1] <= 2)
+        # self.model.rc = pe.Suffix(direction=pe.Suffix.IMPORT_EXPORT)
+        self.set_partitioning_constraints(scheduled_trips, initial_variables_by_commodity)
+        self.set_convexity_constraints(initial_variables_by_commodity)
 
     def add_upper_bound(self, var_index, bound_value):
-        self.model.constraints.add(self.model.x[var_index] <= bound_value)
+        self.model.constraints.add(self.model.var_lambda[var_index] <= bound_value)
 
     def add_lower_bound(self, var_index, bound_value):
-        self.model.constraints.add(self.model.x[var_index] >= bound_value)
+        self.model.constraints.add(self.model.var_lambda[var_index] >= bound_value)
+
+    def set_partitioning_constraints(self, scheduled_trips: list, initial_variables_by_commodity):
+        self.model.partitioning_constraint = pe.ConstraintList()
+        for trip in scheduled_trips:
+            self.model.partitioning_constraint.add(
+                expr=sum(self.model.var_lambda[dk] for dk in self.model.DK
+                         if initial_variables_by_commodity[dk[0]][int(dk[1])].scheduled_trips.__contains__(trip)) == 1)
+
+    def set_convexity_constraints(self, initial_variables_by_commodity):
+        self.model.convexity_constraint = pe.ConstraintList()
+        for commodity in initial_variables_by_commodity.keys():
+            self.model.convexity_constraint.add(
+                expr=sum(self.model.var_lambda[dk] for dk in self.model.DK if dk[0] == commodity) == 1)
 
     def execute(self):
         self.results = self.solver.solve(self.model)
-        # self.model.display()
+        self.model.convexity_constraint.pprint()
+        self.model.partitioning_constraint.pprint()
+
+        self.model.display()
         # self.model.dual.display()
         # self.model.rc.display()
 
@@ -50,21 +70,47 @@ class SetParticioning:
         return self.model.obj()
 
     def get_solution(self):
-        return [self.model.x[j]() for j in self.model.J]
+        if not self.is_feasible():
+            return self.results.solver.status
+        return [self.model.var_lambda[dk]() for dk in self.model.DK]
 
     def evaluate_solution(self, solution):
-        return sum(self.cost_vector[i] * solution[i] for i in self.model.J)
+        return sum(self.cost_vector[dk] * solution[dk] for dk in self.model.DK)
 
     def get_dual(self):
-        duals_by_constraint = {}
+        if not self.is_feasible():
+            return self.results.solver.status
+        duals_by_constraint_by_class = {}
         for constraints in self.model.component_objects(pe.Constraint, active=True):
-            if constraints.name != "constraints":
-                duals_by_constraint[constraints.name] = self.model.dual[constraints]
-        return duals_by_constraint
+            constraint_class = constraints.name
+            duals_by_constraint_by_class[constraint_class] = []
+            for i, constraint in constraints.items():
+                # name = constraint.name
+                duals_by_constraint_by_class[constraint_class].insert(i, self.model.dual[constraint])
+        return duals_by_constraint_by_class
 
     def get_reduced_cost(self):
+        if not self.is_feasible():
+            return self.results.solver.status
         rc = {}
         for var in self.model.component_objects(pe.Var, active=True):
             for i in var:
                 rc[var[i].name] = self.model.rc[var[i]]
         return rc
+
+
+if __name__ == '__main__':
+    trips = ['trip1', 'trip2', 'trip3', 'trip4', 'trip5']
+    lambda_k1_d1 = Variable(15, "D1", [trips[0], trips[1], trips[2]])
+    lambda_k2_d1 = Variable(7, "D1", [trips[2]])
+    lambda_k1_d2 = Variable(12, "D2", [trips[3], trips[4]])
+    lambda_k2_d2 = Variable(8, "D2", [trips[3]])
+    lambda_k3_d2 = Variable(5, "D2", [trips[4]])
+
+    initial_vars = {"D1": [lambda_k1_d1, lambda_k2_d1], "D2": [lambda_k1_d2, lambda_k2_d2, lambda_k3_d2]}
+    relax = SetPartitioning(initial_vars, trips)
+    relax.execute()
+    print('optimal solution: ', relax.get_solution())
+    print('dual values:')
+    for c in relax.get_dual().items():
+        print(str(c))
